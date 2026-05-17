04e76c0df637b460b96f36126647122cf2a51db7
yangyue-l <1695847539@qq.com>
2026-05-17 13:06:24 +0800

    实现网关传递用户信息

--- hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
+++ hm-common/src/main/java/com/hmall/common/interceptors/UserInfoInterceptor.java
@@ -1,5 +1,7 @@
 package com.hmall.common.interceptors;
 
+import cn.hutool.core.util.StrUtil;
+import com.hmall.common.utils.UserContext;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -9,11 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 public class UserInfoInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        //获取登录用户信息
+        String userInfo = request.getHeader("user-info");
+        //判断是否获取了用户，有则存入ThreadLocal
+        if(StrUtil.isNotBlank(userInfo)){
+            UserContext.setUser(Long.valueOf(userInfo));
+        }
+        return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        UserContext.removeUser();
     }
 }
